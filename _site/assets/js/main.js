25c4ead89b49361c34ea105b5a947734ae27a220
Sean Girard <seangirard@yahoo.com>
2014-04-02 13:02:03 -0700

    resources

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -8,13 +8,13 @@ $(function() {
 	
 	$('#feed-scld').gFeed({  
 		url: 'http://www.scld.org/category/business_and_careers/feed/', 
-		max: 3,
+		max: 5,
 		title: '<span class="text-info">SCLD Business Blog</span>'
 	}); 
 
 	$('#feed-spl').gFeed({  
 		url: 'http://blog.spokanelibrary.org/topic/business/feed/', 
-		max: 3,
+		max: 5,
 		title: '<span class="text-success">SPL Business Blog</span>'
 	});
 	
