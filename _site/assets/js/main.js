321a7c4a7165f75150aae89a8c115485b6852fdf
Sean Girard <seangirard@yahoo.com>
2014-04-09 13:02:45 -0700

    rss proxy

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,54 +1,55 @@
+
 $(function() {
-	/*
-	$('a.feed').gFeed( { target: '#feeds'
-											,title: ''
-											,tabs: false
-											,max: 4 } ); 
- 	*/
+
+ 	$.localScroll();
 
  	$.ajaxSetup({
          type: 'POST'
-        ,dataType: 'json'
         ,dataType: 'jsonp'
         ,jsonp: 'callback'
-        /*
-        ,complete: function(obj) {
-          console.log( $.parseJSON(obj.responseText) );
-        }
-        */
       });
-
-	$.localScroll();
-
-	/*
+	
+	var $rss = $('#rss-feed');
+	// proxy rss feeds to json, avoid same origin restrictions
 	$.ajax({ 
 	    url: 'http://api.spokanelibrary.org/v2/feed'
-    , data: { params: { 
-    									url: 'http://www.scld.org/category/business_and_careers/feed/'
+	  , data: { params: { 
+    									limit: 5
+    								, chars: 240
+    								, url: ['http://www.scld.org/category/business_and_careers/feed/'
+    											, 'http://blog.spokanelibrary.org/topic/business/feed/'
+    												]
     									}
     				}
 	  })
 	  .done(function(obj) {  
-	  	console.log(obj);
-			
+  		//console.log(obj);
+			var feeds = { title: 'Business Blogs'
+	  							, data: obj
+	  						}
+	  	var tmpl = Handlebars.compile( $('#rss-feed-tmpl').html() );
+			$rss.hide().html(tmpl(feeds));
 	  })
 	  .fail(function() {
 	  })
 	  .always(function() {
+	  	// sort posts based on timestamp attribute
+	  	var posts = $('div', $rss);
+			var count = 0;
+			posts.sort(function (a, b) {			    
+		    a = parseInt($(a).data('timestamp'), 10);
+		    b = parseInt($(b).data('timestamp'), 10);
+		    count += 2;
+		    if(a < b) {
+		        return 1;
+		    } else if(a > b) {
+		        return -1;
+		    } else {
+		        return 0;
+		    }
+			});
+			$rss.append(posts).fadeIn();
 	  });
-	*/
 
-	$('#feed-scld').gFeed({  
-		url: 'http://www.scld.org/category/business_and_careers/feed/', 
-		max: 5,
-		title: '<span class="text-info">SCLD Business & Careers</span>'
-	}); 
-
-	$('#feed-spl').gFeed({  
-		url: 'http://blog.spokanelibrary.org/topic/business/feed/', 
-		max: 5,
-		title: '<span class="text-success">SPL Business Beat</span>'
-	});
-	
 
 });
